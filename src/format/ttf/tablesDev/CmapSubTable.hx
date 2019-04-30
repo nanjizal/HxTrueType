6edce6a094c5489f81a0285f6713e137ce98ae37
nanjizal <nanjizal@users.noreply.github.com>
2019-04-30 23:04:45 +0100

    tidying prior to fixing

--- src/format/ttf/tablesDev/CmapSubTable.hx
+++ src/format/ttf/tablesDev/CmapSubTable.hx
@@ -1,107 +1,118 @@
 package format.ttf.tables;
 //CMAP
-enum CmapSubTable {
-    Cmap0(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>);
-    Cmap2(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>, subHeaderKeys:Array<Int>, subHeaders:Array<Int>);
-    Cmap4(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>);
-    Cmap6(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>, firstCode:Int);
-    Cmap8(header:CmapHeader, groups:Array<CmapGroup>, is32:Array<Int>);
-    Cmap10(header:CmapHeader, glyphIndexArray:Array<Int>, startCharCode:Int, numChars:Int);
-    Cmap12(header:CmapHeader, groups:Array<CmapGroup>);
-    CmapUnk(header:CmapHeader, bytes:Bytes);
+enum CmapSubEnum{
+    Cmap0(   header:            CmapHeader
+         ,   glyphIndexArray:   Array<GlyphIndex> );
+    Cmap2(   header:            CmapHeader
+         ,   glyphIndexArray:   Array<GlyphIndex>
+         ,   subHeaderKeys:     Array<Int>
+         ,   subHeaders:        Array<Int> );
+    Cmap4(   header:            CmapHeader
+         ,   glyphIndexArray:   Array<GlyphIndex> );
+    Cmap6(   header:            CmapHeader
+         ,   glyphIndexArray:   Array<GlyphIndex>
+         ,   firstCode:         Int );
+    Cmap8(   header:            CmapHeader
+         ,   groups:            Array<CmapGroup>
+         ,   is32:              Array<Int> );
+    Cmap10(  header:            CmapHeader
+          ,  glyphIndexArray:   Array<Int>
+          ,  startCharCode:     Int
+          ,  numChars:          Int );
+    Cmap12(  header:            CmapHeader
+          ,  groups:            Array<CmapGroup> );
+    CmapUnk( header:            CmapHeader
+           , bytes:             Bytes );
+}
+typedef CmapSubData = {
+    cmapSubEnum: CmapSubEnum;
+    
 }
 @:forward
-abstract CmapSubTable( TCmapTable ) to TCmapSubTable { 
+abstract CmapSubTable( cmapSubData: CmapSubData ) to CmapSubData { 
     public
-    function new( :  ){
-        this = ;
+    function new( cmapSubData: CmapSubData  ){
+        this = cmapSubData;
     }
-    
-    function read( bytes, entry: CmapEntry ):CmapSubTable {
-        var input = new BytesInput(bytes);
+    static inline function
+    function read( bytes: Bytes, entry: CmapEntry ): CmapSubTable {
+        var input       = new BytesInput( bytes );
         input.bigEndian = true;
-        input.read(entry.offset);
-
+        input.read( entry.offset );
         var cmapFormat = input.readUInt16();
-        var length = input.readUInt16();
-        var language = input.readUInt16();
-
-        var cmapHeader = {
-            platformId: entry.platformId,
-            platformSpecificId: entry.platformSpecificId,
-            offset: entry.offset,
-            format: cmapFormat,
-            language: language
+        var length     = input.readUInt16();
+        var language   = input.readUInt16();
+        var cmapHeader = { platformId:         entry.platformId
+                         , platformSpecificId: entry.platformSpecificId
+                         , offset:             entry.offset
+                         , format:             cmapFormat
+                         , language:           language
         }
-
         glyphIndexArray = new Array();
-        allGlyphs = new Array();
-
-        if (cmapFormat == 0) {
-            for (j in 0...256)
-                glyphIndexArray[j] = {
-                    charCode: j,
-                    index: input.readByte(),
-                    char: MacGlyphNames.names[j],
+        allGlyphs       = new Array();
+        if( cmapFormat == 0 ){
+            for( j in 0...256 ){
+                glyphIndexArray[ j ] =  { charCode: j
+                                        , index:    input.readByte()
+                                        , char:     MacGlyphNames.names[ j ]
                 };
-            return Cmap0(cmapHeader, glyphIndexArray);
+            }
+            return Cmap0( cmapHeader, glyphIndexArray );
         } else if (cmapFormat == 4) {
-            var segCount = cast input.readUInt16() / 2;
-            var searchRange = input.readUInt16();
-            var entrySelector = input.readUInt16();
-            var rangeShift = input.readUInt16();
-            var endCodes = new Array();
-            var startCodes = new Array();
-            var idDeltas = new Array();
+            var segCount       = cast input.readUInt16() / 2;
+            var searchRange    = input.readUInt16();
+            var entrySelector  = input.readUInt16();
+            var rangeShift     = input.readUInt16();
+            var endCodes       = new Array();
+            var startCodes     = new Array();
+            var idDeltas       = new Array();
             var idRangeOffsets = new Array();
-            var glyphIndices = new Array();
-            for (i in 0...segCount)
-                endCodes.push(input.readUInt16());
+            var glyphIndices   = new Array();
+            for( i in 0...segCount ){
+                endCodes.push( input.readUInt16() );
+            }
             input.readUInt16();
-            for (i in 0...segCount)
-                startCodes.push(input.readUInt16());
-            for (i in 0...segCount)
-                idDeltas.push(input.readUInt16());
-            for (i in 0...segCount)
-                idRangeOffsets.push(input.readUInt16());
+            for( i in 0...segCount ) startCodes.push(     input.readUInt16() );
+            for( i in 0...segCount ) idDeltas.push(       input.readUInt16() );
+            for( i in 0...segCount ) idRangeOffsets.push( input.readUInt16() );
             var count = Std.int((length - (8 * segCount + 16)) / 2);
-            for (i in 0...count)
-                glyphIndices[i] = input.readUInt16();
-
-            glyphIndexArray[0] = {charCode: 0, index: 0, char: String.fromCharCode(0)}; // unknown glyph (missing character)
-            glyphIndexArray[1] = {charCode: 1, index: 1, char: String.fromCharCode(1)}; // null
-            glyphIndexArray[2] = {charCode: 2, index: 2, char: String.fromCharCode(2)}; // carriage return
-            allGlyphs.concat(glyphIndexArray);
-
-            for (i in 0...segCount) {
+            for( i in 0...count ) glyphIndices[ i ] = input.readUInt16();
+            glyphIndexArray[ 0 ] = { charCode: 0, index: 0, char: String.fromCharCode( 0 ) }; // unknown glyph (missing character)
+            glyphIndexArray[ 1 ] = { charCode: 1, index: 1, char: String.fromCharCode( 1 ) }; // null
+            glyphIndexArray[ 2 ] = { charCode: 2, index: 2, char: String.fromCharCode( 2 ) }; // carriage return
+            allGlyphs.concat( glyphIndexArray );
+            for( i in 0...segCount ){
                 // trace('segment '+i+'/'+segCount +' =>  startCode:'+startCodes[i]+',  endCode:'+endCodes[i]+',  idDelta:'+idDeltas[i]+',  idRangeOffset:'+idRangeOffsets[i]);
-                for (j in startCodes[i]...endCodes[i] + 1) {
-                    var index = mapCharCode(j, glyphIndices, segCount, startCodes, endCodes, idRangeOffsets, idDeltas);
+                for( j in startCodes[ i ]...endCodes[i] + 1 ){
+                    var index = mapCharCode( j
+                                           , glyphIndices
+                                           , segCount
+                                           , startCodes
+                                           , endCodes
+                                           , idRangeOffsets
+                                           , idDeltas );
                     // trace('charCode: '+ j + ', char: '+ String.fromCharCode(j)+', index:'+ index);
-                    var glyphIndex:GlyphIndex = {
-                        charCode: j,
-                        index: index,
-                        char: String.fromCharCode(j)
-                    };
-                    glyphIndexArray[j] = glyphIndex;
-                    allGlyphs.push(glyphIndex);
+                    var glyphIndex:GlyphIndex = { charCode: j
+                                                , index:    index
+                                                , char:     String.fromCharCode( j ) };
+                    glyphIndexArray[ j ] = glyphIndex;
+                    allGlyphs.push( glyphIndex );
                 }
             }
-            return Cmap4(cmapHeader, glyphIndexArray);
-        } else if (cmapFormat == 6) {
-            var firstCode:Int = input.readUInt16();
-            var entryCount:Int = input.readUInt16();
-            for (j in 0...entryCount) {
-                var glyphIndex:GlyphIndex = {
-                    charCode: j,
-                    index: input.readUInt16(),
-                    char: MacGlyphNames.names[j]
+            return Cmap4( cmapHeader, glyphIndexArray );
+        } else if ( cmapFormat == 6 ){
+            var firstCode:  Int = input.readUInt16();
+            var entryCount: Int = input.readUInt16();
+            for( j in 0...entryCount ){
+                var glyphIndex: GlyphIndex = { charCode: j
+                                             , index:    input.readUInt16()
+                                             , char:     MacGlyphNames.names[ j ]
                 };
-                glyphIndexArray[j] = glyphIndex;
+                glyphIndexArray[ j ] = glyphIndex;
             }
-            return Cmap6(cmapHeader, glyphIndexArray, firstCode);
+            return Cmap6( cmapHeader, glyphIndexArray, firstCode );
         } else {
-            return CmapUnk(cmapHeader, bytes);
+            return CmapUnk( cmapHeader, bytes );
         }
     }
     public inline
@@ -109,8 +120,13 @@ abstract CmapSubTable( TCmapTable ) to TCmapSubTable {
         
         return o;
     }
-    function mapCharCode(charCode:Int, glyphIndices:Array<Int>, segCount:Int, startCodes:Array<Int>, endCodes:Array<Int>, idRangeOffsets:Array<Int>,
-            idDeltas:Array<Int>):Int {
+    function mapCharCode( charCode:       Int
+                        , glyphIndices:   Array<Int>
+                        , segCount:       Int
+                        , startCodes:     Array<Int>
+                        , endCodes:       Array<Int>
+                        , idRangeOffsets: Array<Int>
+                        , idDeltas:       Array<Int>): Int {
         try {
             for (i in 0...segCount)
                 if (endCodes[i] >= charCode)
--- src/format/ttf/tablesDev/CmapTable.hx
+++ src/format/ttf/tablesDev/CmapTable.hx
@@ -1,10 +1,10 @@
 package format.ttf.tables;
 //CMAP
 enum Platform {
-    Unicode(enc:LangUnicode);
-    Macintosh(enc:LangMacintosh);
+    Unicode(   enc: LangUnicode );
+    Macintosh( enc: LangMacintosh );
     Reserved;
-    Microsoft(enc:LangMicrosoft);
+    Microsoft( enc: LangMicrosoft );
 }
 
 enum LangUnicode {
@@ -56,19 +56,33 @@ enum LangMicrosoft {
 
 // CMAP
 enum CmapSubTable {
-    Cmap0(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>);
-    Cmap2(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>, subHeaderKeys:Array<Int>, subHeaders:Array<Int>);
-    Cmap4(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>);
-    Cmap6(header:CmapHeader, glyphIndexArray:Array<GlyphIndex>, firstCode:Int);
-    Cmap8(header:CmapHeader, groups:Array<CmapGroup>, is32:Array<Int>);
-    Cmap10(header:CmapHeader, glyphIndexArray:Array<Int>, startCharCode:Int, numChars:Int);
-    Cmap12(header:CmapHeader, groups:Array<CmapGroup>);
-    CmapUnk(header:CmapHeader, bytes:Bytes);
+    Cmap0(   header:          CmapHeader
+         ,   glyphIndexArray: Array<GlyphIndex> );
+    Cmap2(   header:          CmapHeader
+         ,   glyphIndexArray: Array<GlyphIndex>
+         ,   subHeaderKeys:   Array<Int>
+         ,   subHeaders:      Array<Int> );
+    Cmap4(   header:          CmapHeader
+         ,   glyphIndexArray: Array<GlyphIndex> );
+    Cmap6(   header:          CmapHeader
+         ,   glyphIndexArray: Array<GlyphIndex>
+         ,   firstCode:       Int );
+    Cmap8(   header:          CmapHeader
+         ,   groups:          Array<CmapGroup>
+         ,   is32:            Array<Int> );
+    Cmap10(  header:          CmapHeader
+          ,  glyphIndexArray: Array<Int>
+          ,  startCharCode:   Int
+          ,  numChars:        Int );
+    Cmap12(  header:          CmapHeader
+          ,  groups:          Array<CmapGroup> );
+    CmapUnk( header:          CmapHeader
+           , bytes:           Bytes );
 }
 typedef CmapGroup = {
-    startCharCode:Int,
-    endCharCode:Int,
-    startGlyphCode:Int
+    startCharCode:  Int,
+    endCharCode:    Int,
+    startGlyphCode: Int
 }
 
 typedef CmapEntry = {
@@ -78,40 +92,42 @@ typedef CmapEntry = {
 }
 
 typedef CmapHeader = {
-    platformId:Int,
-    platformSpecificId:Int,
-    format:Int,
-    offset:Int,
-    language:Int
+    platformId:         Int,
+    platformSpecificId: Int,
+    format:             Int,
+    offset:             Int,
+    language:           Int
 }
+typedef CmapData {
+    directory: Array<CmapEntry>,
+    subTables: Array<CmapSubTable>
+}
+
 @:forward
-abstract CmapTable( TCmapTable ) to TCmapTable { 
+abstract CmapTable( arrCmapSubTable: Array<CmapSubTable> ) to Array<CmapSubTable> { 
     public
     function new( arrCmapSubTable: Array<CmapSubTable> ){
         this = arrCmapSubTable;
     }
-    
     // cmap (character code mapping) table
-    function read( bytes: Bytes ):Array<CmapSubTable> {
-        if (bytes == null)
-            throw 'no cmap table found';
-        var input = new BytesInput(bytes);
-        input.bigEndian = true;
-
-        var version = input.readUInt16();
+    @:from
+    static public inline 
+    function read( bytes: Bytes ): CmapTable {
+        if( bytes == null ) throw 'no cmap table found';
+        var input           = new BytesInput( bytes );
+        input.bigEndian     = true;
+        var version         = input.readUInt16();
         var numberSubtables = input.readUInt16();
-
-        var directory:Array<CmapEntry> = new Array();
-        for (i in 0...numberSubtables) {
-            directory.push({
-                platformId: input.readUInt16(),
-                platformSpecificId: input.readUInt16(),
-                offset: input.readInt32(),
-            });
+        var directory       = new Array<CmapEntry>();
+        for( i in 0...numberSubtables ){
+            directory.push( { platformId:         input.readUInt16()
+                            , platformSpecificId: input.readUInt16()
+                            , offset:             input.readInt32()    } );
         }
-        var subTables:Array<CmapSubTable> = new Array();
-        for (i in 0...numberSubtables) {
-            subTables.push(readSubTable(bytes, directory[i]));
+        var subTables:= new Array<CmapSubTable>();
+        for( i in 0...numberSubtables ){
+            
+            subTables.push( readSubTable( bytes, directory[ i ] ) );
         }
         return subTables;
     }
@@ -134,55 +150,55 @@ abstract CmapTable( TCmapTable ) to TCmapTable {
             buf.add(Type.enumConstructor(subtable));
             buf.add("\n");
 
-            var header = Type.enumParameters(subtable)[0];
-            var platformId:Int = header.platformId;
-            var platformSpecificId:Int = header.platformSpecificId;
-            buf.add('platformId: ');
-            buf.add(platformId);
-            buf.add(' = ');
-            buf.add(Type.getEnumConstructs(Platform)[platformId]);
-            buf.add('\nplatformSpecificId: ');
-            buf.add(platformSpecificId);
-            buf.add(' = ');
-            buf.add(switch (platformSpecificId) {
-                case 0: Type.getEnumConstructs(LangUnicode)[platformSpecificId];
-                case 1: Type.getEnumConstructs(LangMacintosh)[platformSpecificId];
-                case 3: Type.getEnumConstructs(LangMicrosoft)[platformSpecificId];
+            var header                  = Type.enumParameters( subtable )[ 0 ];
+            var platformId: Int         = header.platformId;
+            var platformSpecificId: Int = header.platformSpecificId;
+            buf.add( 'platformId: ' );
+            buf.add( platformId );
+            buf.add( ' = ' );
+            buf.add( Type.getEnumConstructs( Platform )[ platformId ] );
+            buf.add( '\nplatformSpecificId: ' );
+            buf.add( platformSpecificId );
+            buf.add( ' = ' );
+            buf.add( switch ( platformSpecificId ){
+                case 0: Type.getEnumConstructs( LangUnicode )[   platformSpecificId ];
+                case 1: Type.getEnumConstructs( LangMacintosh )[ platformSpecificId ];
+                case 3: Type.getEnumConstructs( LangMicrosoft )[ platformSpecificId ];
                 case _: 'UNSPECIFIED';
             });
 
-            buf.add('\noffset: ');
-            buf.add(header.offset);
-            buf.add('\nformat: ');
-            buf.add(header.format);
-            buf.add('\nlanguage: ');
-            buf.add(header.language);
-            buf.add("\n");
-            switch (subtable) {
+            buf.add( '\noffset: ' );
+            buf.add( header.offset );
+            buf.add( '\nformat: ' );
+            buf.add( header.format );
+            buf.add( '\nlanguage: ' );
+            buf.add( header.language );
+            buf.add( "\n" );
+            switch( subtable ){
                 default:
-                    buf.add("not supported yet\n");
+                    buf.add( "not supported yet\n" );
 
-                case Cmap0(header, glyphIndexArray):
-                    for (j in 0...256) {
-                        buf.add('macintosh CharCode :');
-                        buf.add(j);
-                        buf.add(', index = ');
-                        buf.add(glyphIndexArray[j].index);
-                        buf.add(', char: ');
-                        buf.add(String.fromCharCode(glyphIndexArray[j].charCode));
-                        buf.add("\n");
+                case Cmap0( header, glyphIndexArray ):
+                    for( j in 0...256 ){
+                        buf.add( 'macintosh CharCode :' );
+                        buf.add( j );
+                        buf.add( ', index = ' );
+                        buf.add( glyphIndexArray[ j ].index );
+                        buf.add( ', char: ');
+                        buf.add( String.fromCharCode( glyphIndexArray[ j ].charCode ) );
+                        buf.add( "\n" );
                     }
 
-                case Cmap4(header, glyphIndexArray):
-                    for (j in 0...glyphIndexArray.length)
-                        if (glyphIndexArray[j] != null) {
+                case Cmap4( header, glyphIndexArray ):
+                    for( j in 0...glyphIndexArray.length )
+                        if( glyphIndexArray[ j ] != null ){
                             buf.add('unicode charCode: ');
-                            buf.add(j);
-                            buf.add(', index = ');
-                            buf.add(glyphIndexArray[j].index);
-                            buf.add(', char: ');
-                            buf.add(String.fromCharCode(glyphIndexArray[j].charCode));
-                            buf.add("\n");
+                            buf.add( j );
+                            buf.add( ', index = ' );
+                            buf.add( glyphIndexArray[ j ].index );
+                            buf.add( ', char: ' );
+                            buf.add( String.fromCharCode( glyphIndexArray[ j ].charCode ) );
+                            buf.add( "\n" );
                         }
             }
         }
